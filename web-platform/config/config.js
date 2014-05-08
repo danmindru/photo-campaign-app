a5ca86df4997772c659422485a3e4bbe107dce0d
mindrudan <mindrudan@gmail.com>
2014-05-08 18:57:02 +0200

    switched app to production for AWS deployment

--- web-platform/config/config.js
+++ web-platform/config/config.js
@@ -6,7 +6,7 @@ var _ = require('lodash'),
 // Look for a valid NODE_ENV variable and if one cannot be found load the development NODE_ENV
 process.env.NODE_ENV = ~utilities.walk('./config/env', /(.*)\.js$/).map(function(file) {
 	return file.split('/').pop().slice(0, -3);
-}).indexOf(process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
+}).indexOf(process.env.NODE_ENV) ? process.env.NODE_ENV : 'production';
 
 
 //change the location of the tmp folder (for uploads)
