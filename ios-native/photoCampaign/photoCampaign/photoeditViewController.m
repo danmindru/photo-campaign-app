9ff063f789f01baf9e216a3366fc1af76ce31c74
mindrudan <mindrudan@gmail.com>
2014-05-17 01:16:30 +0200

    improved memory management for photo effects, progress on editing

--- ios-native/photoCampaign/photoCampaign/photoeditViewController.m
+++ ios-native/photoCampaign/photoCampaign/photoeditViewController.m
@@ -8,10 +8,15 @@
 
 #import "photoeditViewController.h"
 
-@interface photoeditViewController ()
+@interface photoeditViewController (){
+	CIContext *currentContext;
+	CIImage *filterResult;
+	UIImage *filteredUIImage;
+}
 
 @property UIImage * photo;
 @property IBOutlet UIImageView * selectedPhotoView;
+@property NSMutableArray * allImages;
 
 @end
 
@@ -40,6 +45,20 @@
 	[self.selectedPhotoView setContentMode:UIViewContentModeScaleAspectFill];
 	//add photo to view
 	[self.selectedPhotoView setImage:self.photo];
+	
+	//move all images types into a dictionary
+	//define filtered images
+	UIImage *tonalImage = [UIImage new];
+	UIImage *noirImage = [UIImage new];
+	UIImage *fadeImage = [UIImage new];
+	UIImage *processImage = [UIImage new];
+	UIImage *monoImage = [UIImage new];
+	UIImage *transferImage = [UIImage new];
+	UIImage *sepiaImage = [UIImage new];
+	self.allImages = [[NSMutableArray alloc] initWithArray:	@[tonalImage, noirImage, fadeImage, processImage, monoImage, transferImage, sepiaImage]];
+	
+	//load all the other photo variations
+	[self loadPhotosWithEffects];
 }
 
 - (void)didReceiveMemoryWarning
@@ -67,46 +86,32 @@
 #pragma mark - Photo filters
 
 - (IBAction)sepiaEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"];
-	
-	[self setFilter:filter];
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:6]];
 }
 
 - (IBAction)transferEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectTransfer"];
-	
-	[self setFilter:filter];
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:5]];
 }
 
 - (IBAction)noirEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectNoir"];
-	
-	[self setFilter:filter];
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:1]];
 }
 
 
 - (IBAction)tonalEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectTonal"];
-	
-	[self setFilter:filter];
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:0]];
 }
 
 - (IBAction)fadeEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectFade"];
-	
-	[self setFilter:filter];
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:2]];
 }
 
 - (IBAction)processEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectProcess"];
-	
-	[self setFilter:filter];
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:3]];
 }
 
-- (IBAction)toneEffect:(id)sender {
-	CIFilter *filter = [CIFilter filterWithName:@"CIPhotoEffectMono"];
-	
-	[self setFilter:filter];
+- (IBAction)monoEffect:(id)sender {
+	[self.selectedPhotoView setImage:[self.allImages objectAtIndex:4]];
 }
 
 
@@ -114,15 +119,38 @@
 	[self.selectedPhotoView setImage:self.photo];
 }
 
-- (void)setFilter:(CIFilter *)filter{
-	CIContext *context = [CIContext contextWithOptions:nil];
-	[filter setValue:[[CIImage alloc] initWithImage:self.photo] forKey:kCIInputImageKey];
+- (void)loadPhotosWithEffects{
+	CIFilter *sepiaFilter = [CIFilter filterWithName:@"CISepiaTone"];
+	CIFilter *transferFilter = [CIFilter filterWithName:@"CIPhotoEffectTransfer"];
+	CIFilter *noirFilter = [CIFilter filterWithName:@"CIPhotoEffectNoir"];
+	CIFilter *tonalFilter = [CIFilter filterWithName:@"CIPhotoEffectTonal"];
+	CIFilter *fadeFilter = [CIFilter filterWithName:@"CIPhotoEffectFade"];
+	CIFilter *processFilter = [CIFilter filterWithName:@"CIPhotoEffectProcess"];
+	CIFilter *monoFilter = [CIFilter filterWithName:@"CIPhotoEffectMono"];
+
+	NSArray *allFilters = [[NSArray alloc] initWithObjects: tonalFilter, noirFilter, fadeFilter, processFilter, monoFilter, transferFilter, sepiaFilter, nil];
+	
+	//run async image load
+	dispatch_queue_t queue = dispatch_queue_create("com.photoCampaign.postImageQueue", NULL);
+	int filterPosition = 0;
+	for(CIFilter *filter in allFilters){
+		dispatch_async(queue, ^{
+			[self.allImages setObject:[self setFilter:filter] atIndexedSubscript:filterPosition];
+		});
+		
+		filterPosition ++;
+	}
+}
+
+- (UIImage *)setFilter:(CIFilter*)currentFilter{
+	currentContext = [CIContext contextWithOptions:nil];
+	[currentFilter setValue:[[CIImage alloc] initWithImage:self.photo] forKey:kCIInputImageKey];
 	//[filter setValue:@0.8f forKey:kCIInputIntensityKey];
-	CIImage* result = [filter valueForKey:kCIOutputImageKey];
-	CGRect extent = [result extent];
-	CGImageRef cgImageRef = [context createCGImage:result fromRect:extent];
-	UIImage* filteredPhoto = [UIImage imageWithCGImage:cgImageRef scale:1 orientation:self.photo.imageOrientation];
-	[self.selectedPhotoView setImage:filteredPhoto];
+	filterResult = [currentFilter valueForKey:kCIOutputImageKey];
+	CGRect extent = [filterResult extent];
+	CGImageRef cgImageRef = [currentContext createCGImage:filterResult fromRect:extent];
+	filteredUIImage = [UIImage imageWithCGImage:cgImageRef scale:1 orientation:self.photo.imageOrientation];
+	return filteredUIImage;
 }
 
 /*
